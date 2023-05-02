af75458dc150c70a7926df62686f80655fe4ea5a
zayyo123 <764439554@qq.com>
2023-05-02 22:54:41 +0800

    监控中台

--- src/componets/HomePage/HomeDivider.vue
+++ src/componets/HomePage/HomeDivider.vue
@@ -1,31 +1,24 @@
-<!--
- * @Author: Ryker
- * @Date: 2022-08-06 09:35:57
- * @LastEditors: 最后编辑
- * @LastEditTime: 2022-08-06 13:52:28
--->
-
 <template>
-<div class="divider"></div>
+  <div class="divider"></div>
 </template>
 
 <script>
 export default {
-    name: 'homeDivider'
+  name: 'homeDivider'
 }
 </script>
 
 <style scoped>
 .divider {
-    display: block;
-    height: 0.1px;
-    width: 100%;
-    font-size: 14px;
-    font-variant: tabular-nums;
-    line-height: 1.5;
-    color: rgba(192, 188, 188, 0.65);
-    box-sizing: border-box;
-    list-style: none;
-    background: rgb(232, 232, 232);
+  display: block;
+  height: 0.1px;
+  width: 100%;
+  font-size: 14px;
+  font-variant: tabular-nums;
+  line-height: 1.5;
+  color: rgba(192, 188, 188, 0.65);
+  box-sizing: border-box;
+  list-style: none;
+  background: rgb(232, 232, 232);
 }
 </style>
